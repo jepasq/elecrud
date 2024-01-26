52f51b0fa992ef99c4328301d877118e8696b84c
Jepasq <jepasq111@gmail.com>
2024-01-26 12:21:28 +0100

    Add a new eudcat unit test for spurious incompatible arguments issue

--- src/eudcat/main_Test.cpp
+++ src/eudcat/main_Test.cpp
@@ -4,6 +4,19 @@
 
 #define EUDC "./eudcat "
 
+/** Exit status returned by eudcat program execution
+  *
+  * Some constants may have the same values as it(s based on the main
+  * program exit values.
+  *
+  */
+namespace exec {
+  constexpr int SUCCESS = 0;          //!< Exit without error
+  constexpr int FILE_NOT_FOUND = 134; //!< Or another exception
+  constexpr int INCOMPATIBLE_ARGS = 134;      //!< Maybe an Invali
+  
+}
+
 /// It seems that running eudcap with options doesn't correctly
 /// consume options even with 
 BOOST_AUTO_TEST_CASE( eudcat_RunningWithOptions )
@@ -11,13 +24,17 @@ BOOST_AUTO_TEST_CASE( eudcat_RunningWithOptions )
   // First, test a normal call works because system() should return
   // command exit status
   int rv = system(EUDC"test.eud");
-  BOOST_REQUIRE_EQUAL( rv, 0 );
+  BOOST_REQUIRE_EQUAL( rv, exec::SUCCESS );
 
   // Then, test with an inexistant project file
   rv = system(EUDC"surelyAnExistantEUDfileIswear.eud");
-  BOOST_REQUIRE_EQUAL( rv, 134 );
+  BOOST_REQUIRE_EQUAL( rv, exec::FILE_NOT_FOUND );
 
   // Now, running on the first file (that exists), shouldn't return 134
   rv = system(EUDC"-a test.eud");
-  BOOST_REQUIRE_NE( rv, 134 );
+  BOOST_REQUIRE_NE( rv, exec::FILE_NOT_FOUND );
+
+  // Now, running on the first file (that exists), shouldn't return 134
+  rv = system(EUDC"-s test.eud");
+  BOOST_REQUIRE_NE( rv, exec::INCOMPATIBLE_ARGS );
 }
