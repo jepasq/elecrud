82482d69dfb58df24be839581096bb1cbbcffd6e
Jepasq <jepasq111@gmail.com>
2024-10-31 09:30:51 +0100

    Add Ok/Cancel buttons to NewCollectionDialog

--- src/editor/NewCollectionDialog.cpp
+++ src/editor/NewCollectionDialog.cpp
@@ -1,9 +1,35 @@
 #include "NewCollectionDialog.hpp"
 
-
+// Map
+/*
+FXDEFMAP(NewCollectionDialog) NewCollectionDialogMap[]={
+  FXMAPFUNC(SEL_COMMAND,NewCollectionDialog::ID_OK,NewCollectionDialog::onCmdOk),
+  FXMAPFUNC(SEL_COMMAND,NewCollectionDialog::ID_CANCEL,NewCollectionDialog::onCmdCancel),
+};
+*/
 
 NewCollectionDialog::NewCollectionDialog(FXWindow *owner):
   FXDialogBox(owner, "New Collection")
 {
+  new FXHorizontalSeparator(this,SEPARATOR_GROOVE|LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X);
+  auto buttons=new FXHorizontalFrame(this,LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X|PACK_UNIFORM_WIDTH|PACK_UNIFORM_HEIGHT,0,0,0,0,0,0,0,0);
+  auto cancelBtn=new FXButton(buttons, "&Canel", NULL, this, ID_CANCEL,
+			      LAYOUT_FILL_Y|LAYOUT_RIGHT);
+  auto okBtn=new FXButton(buttons, "&Ok", NULL, this, ID_OK,
+			  BUTTON_DEFAULT|LAYOUT_CENTER_Y|LAYOUT_RIGHT);
+ 
   resize(600, 400);
 }
+
+long
+NewCollectionDialog::onCmdCancel(FXObject*,FXSelector,void*)
+{
+  return 1;
+}
+
+long
+NewCollectionDialog::onCmdOk(FXObject*,FXSelector,void*)
+{
+  return 1;
+}
+
--- src/editor/NewCollectionDialog.hpp
+++ src/editor/NewCollectionDialog.hpp
@@ -6,7 +6,18 @@
 class NewCollectionDialog : public FXDialogBox
 {
 public:
+  enum
+    {
+      ID_CANCEL=FXDialogBox::ID_LAST,
+      ID_OK,
+      ID_LAST
+    };
+ 
   NewCollectionDialog(FXWindow*);
+
+  long onCmdCancel(FXObject*,FXSelector,void*);
+  long onCmdOk(FXObject*,FXSelector,void*);
+  
 };
 
 #endif // !__NEW_COLLECTION_DIALOG_HPP__
